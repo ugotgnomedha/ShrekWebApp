39efa4a5772e03a2f351ff6a333961597c45055c
Philipp <philvas13@gmail.com>
2021-04-25 17:25:29 +0300

    Initial commit

---
+++ src/main/java/com/example/GreetingController.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) throws SQLException {
+        model.put("name", name);
+        final String url = "jdbc:postgresql://localhost/postgres";
+        final String user = "postgres";
+        final String password = "root";
+        Connection connection = DriverManager.getConnection(url, user, password);
+        ShrekBD shrek = new ShrekBD();
+        shrek.addData(connection);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model){
+        model.put("some", "Hello");
+        return "main";
+    }
+
+}
