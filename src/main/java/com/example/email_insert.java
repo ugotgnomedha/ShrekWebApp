4ffd67d84336cf1881bcd6c5f94e61f152250e28
Philipp <philvas13@gmail.com>
2021-11-17 13:05:48 +0300

    add new maven system

--- src/main/java/com/example/email_insert.java
+++ src/main/java/com/example/email_insert.java
@@ -15,14 +15,16 @@ public class email_insert extends excel_parser {
             for (Row row : sheet) {
                 Cell cell_email = row.getCell(emailColumnIndex);
                 statement.executeUpdate("INSERT INTO jc_contact(email) VALUES('" + cell_email + "') ON CONFLICT (email) DO NOTHING");
-                if (headers.get(i).equals("Email") || headers.get(i).equals("email")){
+                if (headers.get(i).equals("Email") || headers.get(i).equals("email")) {
                     continue;
                 }
                 Cell cell = row.getCell(i);
                 System.out.println(cell.toString());
-                switch (formulaEvaluator.evaluateInCell(cell).getCellType()) {
-                    case Cell.CELL_TYPE_NUMERIC -> statement.executeUpdate("INSERT INTO jc_contact(email, " + headers.get(i) + ") VALUES ('" + cell_email + "', '" + cell.getNumericCellValue() + "') ON CONFLICT (email) DO UPDATE SET " + headers.get(i) + " = '" + cell.getNumericCellValue() + "'");
-                    case Cell.CELL_TYPE_STRING -> statement.executeUpdate("INSERT INTO jc_contact(email, " + headers.get(i) + ") VALUES ('" + cell_email + "', '" + cell.getStringCellValue() + "') ON CONFLICT (email) DO UPDATE SET " + headers.get(i) + " = '" + cell.getStringCellValue() + "'");
+                if (formulaEvaluator.evaluateInCell(cell).getCellType() == Cell.CELL_TYPE_NUMERIC) {
+                    statement.executeUpdate("INSERT INTO jc_contact(email, " + headers.get(i) + ") VALUES ('" + cell_email + "', '" + cell.getNumericCellValue() + "') ON CONFLICT (email) DO UPDATE SET " + headers.get(i) + " = '" + cell.getNumericCellValue() + "'");
+                } else {
+                    statement.executeUpdate("INSERT INTO jc_contact(email, " + headers.get(i) + ") VALUES ('" + cell_email + "', '" + cell.getStringCellValue() + "') ON CONFLICT (email) DO UPDATE SET " + headers.get(i) + " = '" + cell.getStringCellValue() + "'");
+
                 }
             }
         }
