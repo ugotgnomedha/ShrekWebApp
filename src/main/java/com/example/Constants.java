dd5a4e0b017f65ee2c3657fda5887188caecaa12
ugotgnomedha <71843718+ugotgnomedha@users.noreply.github.com>
2022-05-03 12:44:49 +0300

    bug fixes and future updates

--- src/main/java/com/example/Constants.java
+++ src/main/java/com/example/Constants.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class Constants {
 //    public static String url = "jdbc:postgresql://localhost/postgres";
 //    public static String user = "postgres";
-//    public static String password = "root";
+//    public static String password = "gagarin11";
 
-    public static String url = "jdbc:postgresql://ec2-34-253-29-48.eu-west-1.compute.amazonaws.com/dfumj4fkvmbc4r?user=fiykczfrcmpymp&password=2a2a2012e8ededb59604977d4bf427dd80c9984549dbb78f6585b5232890da10&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-    public static String user = "fiykczfrcmpymp";
-    public static String password = "2a2a2012e8ededb59604977d4bf427dd80c9984549dbb78f6585b5232890da10";
+    public static String url = "jdbc:postgresql://ec2-54-247-96-153.eu-west-1.compute.amazonaws.com/d2a08q02s3aa8h?user=drchqhkktdpmjl&password=0c370cd21c839d31d2455acccff0960767e9d4bf321448a58e5a47888a15181d&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+    public static String user = "drchqhkktdpmjl";
+    public static String password = "0c370cd21c839d31d2455acccff0960767e9d4bf321448a58e5a47888a15181d";
 
     public static String mainDataBaseName = "jc_contact";
     public static String temporaryDataBaseName = "MyTemporary";
